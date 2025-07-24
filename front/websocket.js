b3f5052a734b3f893d303e969e65bb144012d773
Egor Siaskov <egor.siaskov@telekom.com>
2025-07-24 10:50:59 +0300

    added two rows and add hover class add

--- front/websocket.js
+++ front/websocket.js
@@ -1,6 +1,9 @@
+const IS_DEV = window.location.hostname === 'localhost';
+const HOST = IS_DEV ? 'ws://localhost:3300' : 'wss://obuba.fun';
+
 class Socket {
     constructor() {
-        this.ws = new WebSocket('wss://obuba.fun');
+        this.ws = new WebSocket(HOST);
         this.ws.onopen = () => {
             [0, 1].forEach(board=> this.sendGetMatrixRequest(board))
         };
@@ -14,11 +17,20 @@ class Socket {
                 console.warn("No matrix provided")
                 return
             } 
-            if (status === 'ok' && (type === 'get' || type === 'update')) {
-                boards[payload.boardIndex].matrix = payload.matrix;
-                renderMatrix(payload.boardIndex, payload.matrix);
+            if (status === 'ok') {
+                if (type === 'get' || type === 'update') {
+                    boards[payload.boardIndex].matrix = payload.matrix;
+                    renderMatrix(payload.boardIndex, payload.matrix);
+                } else if (|| type === 'hover') {
+                    updateHover(payload.boardIndex, payload.isHover)
+                }
             }
         };
+        window.addEventListener('beforeunload', () => {
+            if (this.ws && this.ws.readyState === WebSocket.OPEN) {
+              this.ws.close(1000, 'User closed the page');
+            }
+        });
     }
 
     saveMatrixToServer(boardIndex, matrix) {
@@ -36,6 +48,17 @@ class Socket {
     sendGetMatrixRequest(boardIndex) {
         if (this.ws.readyState === WebSocket.OPEN) {
             this.ws.send(JSON.stringify({ type: 'get', boardIndex }));
+        } else {
+            console.warn('WebSocket not ready');
+        }
+    }
+
+    toggleHover(boardIndex, isHover) {
+        if (this.ws.readyState === WebSocket.OPEN) {
+            this.ws.send(JSON.stringify({ 
+                type: 'hover', 
+                payload: { boardIndex, isHover }
+            }));
         } else {
             console.warn('WebSocket not ready');
         }
